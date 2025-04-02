71ddbf879653f60f11ab2cb0fdc07d61cb77fad6
Dinuka Pramod <dinukah98@gmail.com>
2025-04-02 13:35:44 +0530

    before changing the entire structure

--- src/test/java/com/automation/framework/api/utils/GetApi.java
+++ src/test/java/com/automation/framework/api/utils/GetApi.java
@@ -3,25 +3,23 @@ package com.automation.framework.api.utils;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
+//meka yanna one app/test eke
+//ApiTest.java
+//wrapper clz
 
 public class GetApi {
 
-    //meka yanna one app/test eke
-    //ApiTest.java
     private static final String BASE_URL = ConfigReader.getProperty("base.url");
-    
-    //wrapper clz
-    public static Response getResponseBody(String customEndPoint){
+
+    public static Response getResponseBody(String customEndPoint) {
         return given()
-        .baseUri(BASE_URL)
-        .when()
-        .get(customEndPoint)
-        .then()
-        .extract().response();
+                .baseUri(BASE_URL)
+                .when()
+                .get(customEndPoint)
+                .then()
+                .extract().response();
 
     }
-
-    //post
-
-
 }
+
+// post walatath hadanna
